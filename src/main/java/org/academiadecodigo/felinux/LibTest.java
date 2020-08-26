8e13ee6deacb6b849bcbe37177ba87b1ad6031a0
joel-nevar <nevarthanz@gmail.com>
2020-08-26 16:30:44 +0100

    initialize project, with lib

---
+++ src/main/java/org/academiadecodigo/felinux/LibTest.java
@@ -0,0 +1,9 @@
+package org.academiadecodigo.felinux;
+
+import org.academiadecodigo.simplegraphics.graphics.Rectangle;
+
+public class LibTest {
+    public static void main(String[] args) {
+
+    }
+}
