6aa5317e5b0a4acd6d7c818408e46d456ec00fec
Pikachu <davidmrferreira@hotmail.com>
2020-08-27 18:57:30 +0100

    room with solved size problem

--- src/main/java/org/academiadecodigo/felinux/View/PurgatoryView.java
+++ src/main/java/org/academiadecodigo/felinux/View/PurgatoryView.java
@@ -1,47 +1,11 @@
 package org.academiadecodigo.felinux.View;
 
-import org.academiadecodigo.felinux.GameObjects.drugs.Shroom;
 import org.academiadecodigo.felinux.GameObjects.map.Purgatory;
-import org.academiadecodigo.felinux.View.extras.HighnessMeter;
-
-import static org.academiadecodigo.felinux.GameObjects.map.MapType.PURGATORY;
-import static org.academiadecodigo.felinux.Services.GameCycle.activeMap;
-import static org.academiadecodigo.felinux.Services.GameCycle.imageMap;
-
 
 public class PurgatoryView extends View{
-
 
     public PurgatoryView() {
         super(new Purgatory());
     }
 
-    @Override
-    public void init(HighnessMeter hm) {
-
-        while(player.isAlive() && activeMap == PURGATORY) {
-
-            if (HighnessMeter.meter > 75) {
-                background.setHighEffect();
-            } else {
-                background.setHighEffect();
-            }
-
-            if (firstTime) {
-                imageMap.draw();
-                player.getImage().draw();
-                firstTime = false;
-            }
-
-            hm.animate();
-            map.animate();
-            player.move();
-
-            try {
-                Thread.sleep(30);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    }
 }
