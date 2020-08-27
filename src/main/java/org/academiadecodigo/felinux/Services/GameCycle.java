ecca762a2d745f8a891df6bf8d7970a9f38ce160
Pinhas <codecadet@ada.local>
2020-08-27 17:28:17 +0100

    Working on game cycle

--- src/main/java/org/academiadecodigo/felinux/Services/GameCycle.java
+++ src/main/java/org/academiadecodigo/felinux/Services/GameCycle.java
@@ -3,17 +3,24 @@ package org.academiadecodigo.felinux.Services;
 import org.academiadecodigo.felinux.GameObjects.map.MapType;
 import org.academiadecodigo.felinux.GameObjects.map.Purgatory;
 import org.academiadecodigo.felinux.GameObjects.model.Dorothy;
+import org.academiadecodigo.felinux.View.HallView;
 import org.academiadecodigo.felinux.View.PurgatoryView;
+import org.academiadecodigo.felinux.View.RoomView;
+import org.academiadecodigo.felinux.View.View;
 import org.academiadecodigo.felinux.View.extras.Background;
 import org.academiadecodigo.felinux.View.extras.HighnessMeter;
 import org.academiadecodigo.felinux.controller.PlayerKeyboard;
 import org.academiadecodigo.simplegraphics.pictures.Picture;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class GameCycle {
 
     private Dorothy player;
     public static MapType activeMap = MapType.PURGATORY;
     public static Picture imageMap = new Picture(50,50, activeMap.getSource());
+    private List<View> mapList = new LinkedList<>();
     private PurgatoryView purgatoryView;
     private HighnessMeter highnessMeter;
 
@@ -50,5 +57,23 @@ public class GameCycle {
 
         hallView.init();*/
 
+    }
+
+    /**
+     * Setup for room view
+     */
+    public void setupRoomView() {
+        RoomView roomView = new RoomView();
+        roomView.setPlayer(player);
+        mapList.add(roomView);
+    }
+
+    /**
+     * Setup for hall view
+     */
+    public void setupHallView() {
+        HallView hallView = new HallView();
+        hallView.setPlayer(player);
+        mapList.add(hallView);
     }
 }
